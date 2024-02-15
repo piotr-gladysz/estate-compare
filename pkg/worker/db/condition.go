a564e5b95d8c9969d1c6242d89dc6024eb1c2840
Piotr Gładysz <piotr@develtio.com>
2024-02-15 22:43:54 +0100

    WIP: notifications db

--- pkg/worker/db/condition.go
+++ pkg/worker/db/condition.go
@@ -1,12 +1,21 @@
 package db
 
 import (
+	"context"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Condition struct {
-	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	ID   primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name string             `json:"name" bson:"name"`
+	Wasm []byte             `json:"wasm" bson:"wasm"`
+}
+
+type ConditionRepository interface {
+	Insert(ctx context.Context, condition *Condition) error
+	Delete(ctx context.Context, id primitive.ObjectID) error
+	FindBy(ctx context.Context, by primitive.M) ([]*Condition, error)
 }
 
 type conditionRepository struct {
--- pkg/worker/db/notification.go
+++ pkg/worker/db/notification.go
@@ -1,18 +1,46 @@
 package db
 
 import (
+	"context"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Notification struct {
 	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+
+	ConditionId primitive.ObjectID `json:"conditionId" bson:"conditionId"`
+	Config      map[string]any     `json:"config" bson:"config"`
 }
 
 type NotificationRepository interface {
-	Insert(notification *Notification) error
+	Insert(ctx context.Context, notification *Notification) error
+	Delete(ctx context.Context, id primitive.ObjectID) error
+	FindBy(ctx context.Context, by primitive.M) ([]*Notification, error)
 }
 
 type notificationRepository struct {
 	collection *mongo.Collection
+}
+
+func (r *notificationRepository) Insert(ctx context.Context, notification *Notification) error {
+	res, err := r.collection.InsertOne(ctx, notification)
+	notification.ID = res.InsertedID.(primitive.ObjectID)
+	return err
+}
+
+func (r *notificationRepository) Delete(ctx context.Context, id primitive.ObjectID) error {
+	_, err := r.collection.DeleteOne(ctx, primitive.M{"_id": id})
+	return err
+}
+
+func (r *notificationRepository) FindBy(ctx context.Context, by primitive.M) ([]*Notification, error) {
+	var notifications []*Notification
+	cursor, err := r.collection.Find(ctx, by)
+	if err != nil {
+		return nil, err
+	}
+
+	err = cursor.All(nil, &notifications)
+	return notifications, err
 }
